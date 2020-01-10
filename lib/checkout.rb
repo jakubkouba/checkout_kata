910ac15d4b837f3a3a5892d92c33121bd4a3575d
Jakub Adler <adler.jakub@gmail.com>
2020-01-10 22:20:42 +0000

    Price is 0 when items are empty

---
+++ lib/checkout.rb
@@ -0,0 +1,11 @@
+class Checkout
+
+  def initialize(price_rules)
+    @price_rules = price_rules
+  end
+
+  def total
+    0
+  end
+
+end
--- spec/test_price_spec.rb
+++ spec/test_price_spec.rb
@@ -1,4 +1,19 @@
+require_relative '../lib/checkout'
+
 RSpec.describe 'Test Price' do
 
-  
+  subject(:price) do
+    items.split(//) { |item| checkout.scan(item) }
+    checkout.total
+  end
+
+  let(:checkout) { Checkout.new(price_rules) }
+  let(:price_rules) {}
+  let(:items) {}
+
+  describe 'when items are empty' do
+    let(:items) { '' }
+    it { is_expected.to eq 0 }
+  end
+
 end
