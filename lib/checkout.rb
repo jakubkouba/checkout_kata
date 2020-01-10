c8d4da8d777519ac4881c8bd31741fc530209ebf
Jakub Adler <adler.jakub@gmail.com>
2020-01-10 22:47:18 +0000

    Price is 50 when items contain A

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,21 @@
+require 'byebug'
+
 class Checkout
+
+  attr_reader :items
 
   def initialize(price_rules)
     @price_rules = price_rules
+    @items = []
+  end
+
+  def scan(item)
+    @items << item
   end
 
   def total
-    0
+    return 0 if items.empty?
+    50
   end
 
 end
--- spec/test_price_spec.rb
+++ spec/test_price_spec.rb
@@ -1,19 +1,42 @@
 require_relative '../lib/checkout'
+require 'byebug'
 
 RSpec.describe 'Test Price' do
 
   subject(:price) do
-    items.split(//) { |item| checkout.scan(item) }
+    items.split(//).each { |item| checkout.scan(item) }
     checkout.total
   end
 
   let(:checkout) { Checkout.new(price_rules) }
-  let(:price_rules) {}
+  let(:price_rules) do
+    {
+      'A' => {
+        unit_price: 50,
+        spectial_price: { 3 => 150 }
+      },
+      'B' => {
+        unit_price: 50,
+        spectial_price: { 2 => 45 }
+      },
+      'C' => {
+        unit_price: 20
+      },
+      'D' => {
+        unit_price: 15
+      }
+    }
+  end
   let(:items) {}
 
   describe 'when items are empty' do
     let(:items) { '' }
     it { is_expected.to eq 0 }
+  end
+
+  describe 'when items contain A' do
+    let(:items) { 'A' }
+    it { is_expected.to eq 50 }
   end
 
 end
